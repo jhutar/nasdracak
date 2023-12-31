994cb8430be25fcf7f7da9badd72f3164849cdb4
Jan Hutar <jhutar@seznam.cz>
2023-12-31 23:06:41 +0100

    feat: Taake generuj variantu bez prehazeni

--- build-gamebook.sh
+++ build-gamebook.sh
@@ -13,6 +13,10 @@ rm -f gamebook/opustena_magova_vez.dot.png
 pandoc --lua-filter=pangamebook/pangamebook.lua --pdf-engine=xelatex -o gamebook/opustena_magova_vez.pdf gamebook/opustena_magova_vez.md
 echo "PDF doc: gamebook/opustena_magova_vez.pdf"
 
+# Raw PDF doc
+pandoc -Mgamebook-shuffle=false -Mgamebook-pre-link="[[" -Mgamebook-post-link="]]" -Mgamebook-numbers=false --lua-filter=pangamebook/pangamebook.lua --pdf-engine=xelatex -o gamebook/opustena_magova_vez-raw.pdf gamebook/opustena_magova_vez.md
+echo "Raw PDF doc: gamebook/opustena_magova_vez-raw.pdf"
+
 # PNG graph
 pandoc --lua-filter=pangamebook/pangamebook.lua -o gamebook/opustena_magova_vez2.md gamebook/opustena_magova_vez.md
 pandoc --lua-filter=pangamebook/pangamebookdot.lua -t plain -o gamebook/opustena_magova_vez.dot gamebook/opustena_magova_vez2.md
