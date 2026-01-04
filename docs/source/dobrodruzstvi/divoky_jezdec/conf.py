3aa0df1aa9ed39e0f6520a6fa231e628a06ea161
Jan Hutar <jhutar@seznam.cz>
2026-01-04 09:33:03 +0100

    fix: Chybejici conf.py

---
+++ docs/source/dobrodruzstvi/divoky_jezdec/conf.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append("../../")
+from conf import *
+
+project = 'Dobrodružství "Divoký jezdec"'
+release = '0.1'
+latex_documents = [('index', 'nasdracak-dobrodruzstvi-divoky_jezdec.tex', project, author, latex_theme, False)]
